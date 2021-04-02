0da3c0ffd8d14319d01b217029c247d3b53bc69f
Daine Marshall <dmarshall@thelightsource.com>
2021-04-02 14:18:04 -0400

    working gallery pages with new images

--- routes/index.js
+++ routes/index.js
@@ -1,18 +1,6 @@
 const express = require('express');
 const router = express.Router();
 const fs = require('fs')
-const filePath = '../public/javascripts/gallery'
-
-
-const readJson = (path, cb) => {
-  fs.readFile(require.resolve(path), (err, data) => {
-    if (err) {
-      cb(err)
-    } else {
-      cb(null, JSON.parse(data))
-    }
-  })
-}
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -21,58 +9,24 @@ router.get('/', function(req, res, next) {
       'folder': 'folderName',
       'imageName': 'exampleName'
     }
-  ]
-
-  
+  ] 
 
   res.render('index', { title: 'Express', allImages});
 });
 
 router.get('/gallery/:gallery', function(req, res, next) {
   const galleryName = req.params.gallery
-  readJson(filePath, (err, galleries) => {
-    // let images = Object.keys(galleries[galleryName])
-    const imageNames = []
-    let images = []
-    // check if any images exist in gallery
-    console.log(galleries[galleryName].length)
-    if (galleries[galleryName].length > 0) {
-      images = galleries[galleryName]
-
-      if(images.length){
-        images.forEach(image => {
-          imageNames.push({'imageName':image.imageName})
-        });
-      }
-      res.render('index', { title: req.params.gallery, galleryImages:imageNames, gallerySelected: galleryName });
-    }
-    else {
-      imageNames.push({'imageName':'default.jpg'});
-      res.render('index', { title: req.params.gallery, galleryImages:imageNames, gallerySelected:"default" })
-    }
+  const imageNames = []
+  fs.readdirSync(`./public/images/${galleryName}`).forEach(imageName => {
+    console.log(imageName);
+    imageNames.push({'imageName':imageName})
   })
 
-  
+  res.render('index', { title: req.params.gallery, galleryImages:imageNames, gallerySelected: galleryName }); 
 });
-
-// router.get('/kids', function(req, res, next) {
-//   res.render('index', { title: 'kids' });
-// });
-
-// router.get('/newborn', function(req, res, next) {
-//   res.render('index', { title: 'newborn' });
-// });
 
 router.get('/about', function(req, res, next) {
   res.render('index', { title: 'about' });
 });
-
-// router.get('/petsevents', function(req, res, next) {
-//   res.render('index', { title: 'pets and events' });
-// });
-
-// router.get('/smokebombs', function(req, res, next) {
-//   res.render('index', { title: 'smokebombs' });
-// });
 
 module.exports = router;
